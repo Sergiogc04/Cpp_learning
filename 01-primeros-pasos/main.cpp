6ce995aad9cb40471e65820c3393f95192762053
sergarcab <sergarcab@alum.us.es>
2026-09-15 12:35:28 +0200

    Inicio del aprendizaje de C++

---
+++ 01-primeros-pasos/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::vector<std::string> plagas{"weevil", "snail", "cutworm", "grasshopper"};
+
+    std::sort(plagas.begin(), plagas.end(),
+              [](const std::string& a, const std::string& b) {
+                  return a.size() < b.size();
+              });
+
+    for (const auto& p : plagas) {
+        std::cout << p << " (" << p.size() << " chars)\n";
+    }
+    
+    std::cout << plagas[10] << "\n";
+}
